07584b733cd119b7d92e0a3694faf2f98e8d7898
Robert Tidball <info@fxmacrodata.com>
2026-07-08 23:46:35 +1000

    Add FXMacroData release calendar integration

--- gym_anytrading/datasets/__init__.py
+++ gym_anytrading/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .utils import load_dataset as _load_dataset
+from .fxmacrodata import load_fxmacrodata_calendar
 
 
 # Load FOREX datasets
@@ -6,3 +7,9 @@ FOREX_EURUSD_1H_ASK = _load_dataset('FOREX_EURUSD_1H_ASK', 'Time')
 
 # Load Stocks datasets
 STOCKS_GOOGL = _load_dataset('STOCKS_GOOGL', 'Date')
+
+__all__ = [
+    'FOREX_EURUSD_1H_ASK',
+    'STOCKS_GOOGL',
+    'load_fxmacrodata_calendar',
+]
