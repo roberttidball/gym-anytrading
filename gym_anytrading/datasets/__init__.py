9fbf035313880580d43355498130ac610f36946a
Robert Tidball <info@fxmacrodata.com>
2026-07-09 00:27:50 +1000

    Expand FXMacroData read endpoint coverage

--- gym_anytrading/datasets/__init__.py
+++ gym_anytrading/datasets/__init__.py
@@ -1,15 +1,26 @@
 from .utils import load_dataset as _load_dataset
-from .fxmacrodata import load_fxmacrodata_calendar
-
+from .fxmacrodata import (
+    FXMacroDataClient,
+    load_fxmacrodata_announcements,
+    load_fxmacrodata_calendar,
+    load_fxmacrodata_dataset,
+    load_fxmacrodata_forex,
+    load_fxmacrodata_predictions,
+)
 
 # Load FOREX datasets
-FOREX_EURUSD_1H_ASK = _load_dataset('FOREX_EURUSD_1H_ASK', 'Time')
+FOREX_EURUSD_1H_ASK = _load_dataset("FOREX_EURUSD_1H_ASK", "Time")
 
 # Load Stocks datasets
-STOCKS_GOOGL = _load_dataset('STOCKS_GOOGL', 'Date')
+STOCKS_GOOGL = _load_dataset("STOCKS_GOOGL", "Date")
 
 __all__ = [
-    'FOREX_EURUSD_1H_ASK',
-    'STOCKS_GOOGL',
-    'load_fxmacrodata_calendar',
+    "FOREX_EURUSD_1H_ASK",
+    "STOCKS_GOOGL",
+    "FXMacroDataClient",
+    "load_fxmacrodata_announcements",
+    "load_fxmacrodata_calendar",
+    "load_fxmacrodata_dataset",
+    "load_fxmacrodata_forex",
+    "load_fxmacrodata_predictions",
 ]
